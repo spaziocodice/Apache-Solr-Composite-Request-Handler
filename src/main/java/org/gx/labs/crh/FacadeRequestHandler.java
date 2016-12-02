c2da9198e379a6d5bccfea5e1b0738fe45aa260b
agazzarini <a.gazzarini@gmail.com>
2016-12-02 08:15:25 +0100

    Impl works / Test suite not yet completed

--- src/main/java/org/gx/labs/crh/FacadeRequestHandler.java
+++ src/main/java/org/gx/labs/crh/FacadeRequestHandler.java
@@ -2,18 +2,24 @@ package org.gx.labs.crh;
 
 import static java.util.Arrays.stream;
 
+import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
+import org.apache.solr.common.util.SimpleOrderedMap;
 import org.apache.solr.core.SolrCore;
 import org.apache.solr.handler.RequestHandlerBase;
 import org.apache.solr.handler.component.SearchHandler;
 import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.request.SolrQueryRequestBase;
 import org.apache.solr.request.SolrRequestHandler;
 import org.apache.solr.response.BasicResultContext;
 import org.apache.solr.response.ResultContext;
 import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.search.DocList;
 import org.apache.solr.search.DocSlice;
+import org.apache.solr.util.RTimerTree;
+import org.eclipse.jetty.http.HttpParser.RequestHandler;
 
 /**
  * A {@link SolrRequestHandler} that subsequently invokes several children {@link SolrRequestHandler}s.
@@ -22,13 +28,18 @@ import org.apache.solr.search.DocSlice;
  * @since 1.0
  */
 public class FacadeRequestHandler extends RequestHandlerBase {
+	private final static DocList EMPTY_DOCLIST = new DocSlice(0, 0, new int[0], new float[0], 0, 0f);
+	
+	final static String RESPONSE_KEY = "response"; // If only SolrQueryResponse.RESPONSE_KEY would be public ;)
+	final static String RESPONSE_HEADER_KEY = "responseHeader"; // If only SolrQueryResponse.RESPONSE_HEADER_KEY would be public ;)
+	
 	private String [] chain;
 	
-	@SuppressWarnings("rawtypes")
 	@Override
-	 public void init(final NamedList args) {
+	@SuppressWarnings("rawtypes")
+	public void init(final NamedList args) {
 		chain = SolrParams.toSolrParams(args).get("chain").split(",");
-	 }
+	}
 	
 	@Override
 	public void handleRequestBody(
@@ -36,38 +47,69 @@ public class FacadeRequestHandler extends RequestHandlerBase {
 			final SolrQueryResponse response) throws Exception {
 		final SolrParams params = request.getParams();
 
-		response.addResponse(
+		response.setAllValues(
 			stream(chain)
-				.map(refName -> { return requestHandler(request, refName);})
+				.map(refName -> { return requestHandler(request, refName); })
 				.filter(SearchHandler.class::isInstance) 
 				.map(handler -> { return executeQuery(request, response, params, handler); })
-				.filter(context -> context.getDocList().size() > 0)
+				.filter(qresponse -> howManyFound(qresponse) > 0)
 				.findFirst()
-				.orElse(new BasicResultContext(
-							new DocSlice(0, 0, new int[0], new float[0], 0, 0f), 
-							response.getReturnFields(),
-							request.getSearcher(),
-							null,
-							request)));
+				.orElse(emptyResponse(request, response)));
+	}
+	
+	/**
+	 * Returns the total count of matches associated with the given query response.
+	 * 
+	 * @param qresponse the "response" portion of the {@link QueryResponse}.
+	 * @return the total count of matches associated with the given query response.
+	 */
+	int howManyFound(final NamedList<?> qresponse) {
+		return ((ResultContext)qresponse.get(RESPONSE_KEY)).getDocList().size();
+	}
+	
+	/**
+	 * Returns a Null Object response, indicating no handler produced a match.
+	 * 
+	 * @param request the current {@link SolrQueryRequest}.
+	 * @param response the current {@link SolrQueryResponse}.
+	 * @return a Null Object response, indicating no handler produced a match.
+	 */
+	NamedList<Object> emptyResponse(final SolrQueryRequest request, final SolrQueryResponse response) {
+		final SimpleOrderedMap<Object> empty = new SimpleOrderedMap<>();
+		empty.add(RESPONSE_KEY, 
+				new BasicResultContext(
+					EMPTY_DOCLIST, 
+					response.getReturnFields(),
+					request.getSearcher(),
+					null,
+					request));
+		
+		empty.add(RESPONSE_HEADER_KEY, new SimpleOrderedMap<>());
+		return empty;
 	}
 	
 	/**
 	 * Executes the given handler (query) logic.
 	 * @param request the current {@link SolrQueryRequest}.
 	 * @param response the current {@link SolrQueryResponse}.
-	 * @param params
-	 * @param handler
-	 * @return
+	 * @param params the request parameters.
+	 * @param handler the executor handler.
+	 * @return the query response, that is, the result of the handler's query execution.
 	 */
-	ResultContext executeQuery(
+	@SuppressWarnings("unchecked")
+	NamedList<Object> executeQuery(
 			final SolrQueryRequest request, 
 			final SolrQueryResponse response, 
 			final SolrParams params, 
 			final SolrRequestHandler handler) {
-		request.setParams(new ModifiableSolrParams(params));
 		final SolrQueryResponse scopedResponse = clone(response);
-		handler.handleRequest(request, scopedResponse); 
-		return ((ResultContext)scopedResponse.getResponse());	
+		handler.handleRequest(
+				new SolrQueryRequestBase(
+						request.getCore(), 
+						new ModifiableSolrParams(params), 
+						new RTimerTree()) {}, 
+				scopedResponse); 
+		return scopedResponse.getValues();	
 	}
 	
 	/**
@@ -77,7 +119,9 @@ public class FacadeRequestHandler extends RequestHandlerBase {
 	 * @return a clone of the incoming response.
 	 */
 	public SolrQueryResponse clone(final SolrQueryResponse response) {
-		return new SolrQueryResponse();
+		final SolrQueryResponse clone = new SolrQueryResponse();
+		clone.addResponseHeader(response.getResponseHeader());
+		return clone;
 	}
 	
 	/**
--- src/test/java/org/gx/labs/crh/FacadeRequestHandlerTestCase.java
+++ src/test/java/org/gx/labs/crh/FacadeRequestHandlerTestCase.java
@@ -1,11 +1,16 @@
-package org.gx.labs.crh.unit;
+package org.gx.labs.crh;
 
 import static org.mockito.Mockito.mock;
 
-import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.common.util.SimpleOrderedMap;
 import org.apache.solr.request.SolrRequestHandler;
-import org.gx.labs.crh.FacadeRequestHandler;
+import org.apache.solr.response.SolrQueryResponse;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * TODO : INCOMPLETE
@@ -27,6 +32,7 @@ public class FacadeRequestHandlerTestCase {
 		rh2 = mock(SolrRequestHandler.class);
 		rh3 = mock(SolrRequestHandler.class);
 		
+		cut = new FacadeRequestHandler();
 //		cut = new FacadeRequestHandler() {
 //			@Override
 //			SolrRequestHandler requestHandler(final SolrQueryRequest request, final String name) {
@@ -41,6 +47,21 @@ public class FacadeRequestHandlerTestCase {
 //				throw new IllegalArgumentException("Unknown request handler: " + name);
 //			}
 //		};
+	}
+	
+	@Test
+	public void cloneResponse() {
+		final Object response = new Object();
+		final NamedList<Object> responseHeader = new SimpleOrderedMap<>();
+		
+		final SolrQueryResponse qresponse = mock(SolrQueryResponse.class);
+		when(qresponse.getResponse()).thenReturn(response);
+		when(qresponse.getResponseHeader()).thenReturn(responseHeader);
+		
+		final SolrQueryResponse clone = cut.clone(qresponse);
+		
+		assertNull(clone.getResponse());
+		assertSame(responseHeader, clone.getResponseHeader());
 	}
 	
 	public void stopAtFirstNonZeroResult() {
